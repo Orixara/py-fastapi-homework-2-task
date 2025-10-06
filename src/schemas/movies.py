454ef2e1dbabc7975ee46ad2769064674bc94d76
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-10-06 17:32:35 +0300

    added all schemas to schemas/movies.py

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,100 @@
-# Write your code here
+from pydantic import BaseModel, Field, field_validator, ConfigDict
+from typing import List
+from datetime import date, datetime
+
+from database.models import MovieStatusEnum
+
+
+class GenreSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+
+
+class ActorSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+
+class LanguageSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+
+
+class CountrySchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    code: str
+    name: str | None = None
+
+
+class MovieListItemSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+    date: date
+    score: float
+    overview: str
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieListItemSchema]
+    prev_page: str | None = None
+    next_page: str | None = None
+    total_pages: int
+    total_items: int
+
+
+class MovieDetailSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+    date: date
+    score: float
+    overview: str
+    status: MovieStatusEnum
+    budget: float
+    revenue: float
+    country: CountrySchema
+    genres: List[GenreSchema]
+    actors: List[ActorSchema]
+    languages: List[LanguageSchema]
+
+
+class MovieCreateSchema(BaseModel):
+    name: str = Field(..., max_length=255)
+    date: date
+    score: float = Field(..., ge=0, le=100)
+    overview: str
+    status: MovieStatusEnum
+    budget: float = Field(..., ge=0)
+    revenue: float = Field(..., ge=0)
+    country: str = Field(..., max_length=3)
+    genres: List[str]
+    actors: List[str]
+    languages: List[str]
+
+    @field_validator("date")
+    @classmethod
+    def validate_date_not_too_future(cls, value):
+        one_year_from_now = datetime.now().date().replace(year=datetime.now().year + 1)
+        if value > one_year_from_now:
+            raise ValueError("Date must not be more than one year in the future.")
+        return value
+
+
+class MovieUpdateSchema(BaseModel):
+    name: str | None = Field(None, max_length=255)
+    date: date | None = None
+    score: float | None = Field(None, ge=0, le=100)
+    overview: str | None = None
+    status: MovieStatusEnum | None = None
+    budget: float | None = Field(None, ge=0)
+    revenue: float | None = Field(None, ge=0)
