ee598a6bed4b79d7bd088b66ae94a795999871fe
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-10-06 18:27:21 +0300

    fixed flake8

--- src/main.py
+++ src/main.py
@@ -1,23 +1,11 @@
-from fastapi import FastAPI, Request
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
 
 from routes import movie_router
 
 
-app = FastAPI(
-    title="Movies homework",
-    description="Description of project"
-)
-
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=400,
-        content={"detail": "Invalid input data."}
-    )
-
+app = FastAPI(title="Movies homework", description="Description of project")
 api_version_prefix = "/api/v1"
 
-app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
+app.include_router(
+    movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"]
+)
